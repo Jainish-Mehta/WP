15f067c6b82cb2865a28c73fb8a640f790e25b51
Jainish-Mehta <jainishmehta2005@outlook.com>
2026-03-20 19:25:30 +0530

    Add initial implementation for cookie handling in PHP

---
+++ Unit-4 Server Side Programming with PHP/30.Cookies/cookies_create_access_delete.php
@@ -0,0 +1,11 @@
+<?php
+setcookie("Key", "Value", time() + 3600);
+
+if (isset($_COOKIE["Key"])) {
+    echo "Cookie Value: " . $_COOKIE["Key"];
+} else {
+    echo "Cookie is not set.";
+}
+
+setcookie("Key", "", time() - 3600);
+?>
